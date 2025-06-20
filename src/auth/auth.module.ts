31114ecda68c94c8f39b8b758841efd0ea8482f3
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-06-20 09:47:48 -0300

    feat: import authController

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthController } from './dto/auth.dto';
+import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants/constants';
 import { UsersModule } from 'src/users/users.module';
